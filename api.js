ba342e91bbb780c7e8227c9362f6e9094bcc62e4
ImportSubham <126438490+ImportSubham@users.noreply.github.com>
2025-12-27 10:29:01 +0530

    Initialize API module with axios and auth token setup

---
+++ api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const API = `${BACKEND_URL}/api`;
+
+export const api = axios.create({
+  baseURL: API,
+});
+
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common['Authorization'];
+     }
+};
